7fac9e5fb2a5c9443d08b1ccaca9e062f2b6ec29
cdvines <cdvines@iu.edu>
2021-11-02 14:21:44 -0400

    getting started on setup

---
+++ src/app-router.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      <p>Home page of black female auteurs</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/routes.js
@@ -0,0 +1,1 @@
+export const HOME = "/black-female-auteurs"
