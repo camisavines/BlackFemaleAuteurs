8e1ab1628b09e3f7fe82904e0470c3038c29e567
cdvines <cdvines@iu.edu>
2021-11-08 21:54:41 -0500

    starting

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ import * as ROUTES from "../constants/routes";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg w-100 navbar-dark" style={{fontSize: "10pt"}}>
+        <nav className="navbar navbar-expand-lg w-100 navbar-light" style={{fontSize: "10pt"}}>
             <Link className="navbar-brand" to={ROUTES.HOME}>BFA</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
