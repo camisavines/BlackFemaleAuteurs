d383bed4e39eb230a88ce5b64f128fbdf8ded4db
cdvines <cdvines@iu.edu>
2021-11-16 14:12:37 -0500

    starting on first film annotation

--- src/components/research/BeyonceLemonade.js
+++ src/components/research/BeyonceLemonade.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 const BeyonceLemonade = () => {
-    const TITLE = "Lemonade Video Album";
-    const AUTEUR = "Beyoncé";
-    const RELEASE_YEAR = "2012";
+    const TITLE = "Lemonade";
+    const AUTEUR = "Beyoncé Knowles-Carter";
+    const RELEASE_YEAR = "2016";
+    const FILM_TYPE = "Video Album"
+    const RUNTIME = "1h 22m"
+    const RELEASE_DATE = "April 23, 2016";
 
 
     return (
         <div className="tl-item">
             <div className="tl-dot b-primary"></div>
             <div className="tl-content">
-                <h3>{TITLE} </h3>
-                <p>{AUTEUR}</p>
-                <p className="tl-date text-muted mt-1">{RELEASE_YEAR}</p>
+                <h3>{TITLE}</h3>
+                <div>{AUTEUR}</div>
+                <div className="tl-date text-muted mt-1">{RELEASE_YEAR} • {FILM_TYPE} • {RUNTIME}</div>
 
 
 
